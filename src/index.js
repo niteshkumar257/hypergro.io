3a90be13077340862ab6e1f48f033250465f13e3
niteshku25 <nitesh@bigcircle.tech>
2024-02-10 23:00:20 +0530

    updated the index.js file

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ console.log("nitesh kumar");
 console.log("akanchha kumari");i
 console.log("ladi off");
 console.log("I am down but not out");
+console.log("git log patch");
+console.log("second patch");
+console.log("third patch");
